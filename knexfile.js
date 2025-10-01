1842eac4da6405b044af6fae581f6a0ccd5baf02
HECTOR GABRIEL ERMIDEZ PEREZ GONZALEZ <hgperezgo@correo.url.edu.gt>
2025-09-30 20:32:55 -0600

    Update Knexfile and Docker-compose.yml

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,33 @@
 module.exports = {
-    development: {
-        client: 'mysql2',
-        connection: {
-            host: '127.0.0.1',
-            port:3307,
-            user: 'root',
-            password: '7465726D696E61 ',
-            database: 'db_test'
-        },
-        migrations: {
-            directory: './migrations'
-        }
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: '127.0.0.1',
+      port: 3307, // Puerto expuesto en docker-compose
+      user: 'root',
+      password: '7465726D696E61', // Contraseña en docker-compose
+      database: 'term'
     },
-
-    test: {
-        client: 'sqlite3',
-        connection: { filename: ':memory:' },
-        useNullAsDefault: true,
-        migrations: { directory: './migrations' },
-
+    pool: {
+      min: 2,
+      max: 10,
+      idleTimeoutMillis: 60000,      // 60s inactividad
+      createTimeoutMillis: 30000,    // 30s para crear nueva conexión
+      acquireTimeoutMillis: 30000    // 30s para adquirir una conexión
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
     }
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: { filename: ':memory:' },
+    useNullAsDefault: true,
+    migrations: { directory: './migrations' },
+    seeds: { directory: './seeds' }
+  }
 };
